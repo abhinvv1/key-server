4e2a760035fb77d1443fe011b24978ab2b54c200
abhinvv1 <abhinav.p@browserstack.com>
2024-07-26 14:29:48 +0530

    add error handling

--- redis_client.rb
+++ redis_client.rb
@@ -6,5 +6,8 @@ class RedisClient
 
   def connection
     @redis = Redis.new(url: ENV['REDIS_URL'])
+  rescue Redis::CannotConnectError => e
+    @logger.error("Failed to connect to Redis: #{e.message}")
+    raise
   end
 end
